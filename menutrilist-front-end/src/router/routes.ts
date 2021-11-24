f8652237ffd4c4712849158b4569e64a902a6754
Veronica Yose Ardilla <veronica.ardilla@student.matanauniversity.ac.id>
2021-11-25 00:08:55 +0700

    Add pages route

--- menutrilist-front-end/src/router/routes.ts
+++ menutrilist-front-end/src/router/routes.ts
@@ -4,8 +4,18 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/Index.vue') }],
+    children: [
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: 'sign-in', component: () => import('src/pages/user/SignIn.vue')},
+      { path: 'sign-up', component: () => import('src/pages/user/SignUp.vue')},
+      { path: 'forgot-password', component: () => import('src/pages/user/ForgotPassword.vue')},
+      { path: 'search-food', component: () => import('src/pages/site/SearchFood.vue')},
+      { path: 'search-food-dialog', component: () => import('src/components/search-food/DetailDialog.vue')},
+      { path: 'add-to-menu', component: () => import('src/components/search-food/AddToMenu.vue')},
+      { path: 'select-schedule', component: () => import('src/components/schedule/Select.vue')},
+    ],
   },
+
 
   // Always leave this as last one,
   // but you can also remove it
