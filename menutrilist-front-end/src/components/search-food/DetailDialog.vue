229fd98e4e2e8e42ab3252eaa6893a0b4665d0f1
Veronica Yose Ardilla <veronica.ardilla@student.matanauniversity.ac.id>
2021-11-25 00:08:26 +0700

    Add component

---
+++ menutrilist-front-end/src/components/search-food/DetailDialog.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page>
+      <div class="justify-center q-mt-xl">
+      <h3 class="text-bold text-center q-my-sm">Detail Food</h3>
+      <h3 class="text-bold text-center text-primary q-mt-lg q-mb-none">Nutrition List</h3>
+      <base-card class="q-ma-lg">
+        <q-card-section>
+            Protein
+        </q-card-section>
+      </base-card>
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent} from 'vue';
+import BaseCard from 'components/ui/BaseCard.vue';
+
+export default defineComponent({
+  components: {
+    BaseCard,
+  },
+  setup() {
+
+
+    return {
+    };
+  }
+});
+</script>
